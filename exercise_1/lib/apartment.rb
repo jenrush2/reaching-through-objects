1aac270e9aa803cb0d4ea2a729fd2994c840923d
Jennifer Rush <jennifer.rush2@gmail.com>
2022-12-12 20:49:58 -0700

    Create add room and list alphabetically

--- exercise_1/lib/apartment.rb
+++ exercise_1/lib/apartment.rb
@@ -1,14 +1,24 @@
 require './lib/room'
 
 class Apartment
+    attr_reader :rooms
     attr_accessor :is_rented
 
     def initialize(is_rented=false)
         @is_rented = is_rented
+        @rooms = []
     end
 
     def is_rented?
         @is_rented
+    end
+
+    def add_room(room_name)
+        @rooms << room_name.name
+    end
+
+    def list_rooms_by_name_alphabetically
+        rooms.sort
     end
 
 end
