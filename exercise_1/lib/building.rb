f7fb454a26825b26b545495ff02837a2ec657365
Jennifer Rush <jennifer.rush2@gmail.com>
2022-12-14 19:09:24 -0700

    Create add apt and list apt

--- exercise_1/lib/building.rb
+++ exercise_1/lib/building.rb
@@ -2,10 +2,21 @@ require './apartment'
 require './room'
 
 class Building
-    attr_reader :building_number, :building_name
+    attr_reader :building_number, 
+                :building_name 
+    attr_accessor :apartments
 
     def initialize(building_number, building_name)
         @building_number = building_number
         @building_name = building_name
+        @apartments = []
+    end
+
+    def add_apartment(new_apartment)
+        @apartments << new_apartment
+    end
+
+    def list_apartments
+        @apartments
     end
 end
