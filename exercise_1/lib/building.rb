0198300fa42315e6d114419707d7a1b81b6ded69
Jennifer Rush <jennifer.rush2@gmail.com>
2022-12-14 18:57:28 -0700

    Initialize class Building

--- exercise_1/lib/building.rb
+++ exercise_1/lib/building.rb
@@ -1,3 +1,11 @@
 require './apartment'
 require './room'
 
+class Building
+    attr_reader :building_number, :building_name
+
+    def initialize(building_number, building_name)
+        @building_number = building_number
+        @building_name = building_name
+    end
+end
