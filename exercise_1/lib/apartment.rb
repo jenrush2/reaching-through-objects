0ee50c8a1c23bc3fad62da0383dabbc79ba35ba9
Jennifer Rush <jennifer.rush2@gmail.com>
2022-12-14 18:41:51 -0700

    Add room limit

--- exercise_1/lib/apartment.rb
+++ exercise_1/lib/apartment.rb
@@ -1,21 +1,26 @@
 require './lib/room'
 
 class Apartment
-    attr_reader :rooms
-    attr_accessor :is_rented
+    attr_accessor :is_rented, :rooms, :room_count
 
     def initialize(is_rented=false)
         @is_rented = is_rented
         @rooms = []
+        @room_count = 0
     end
 
     def is_rented?
         @is_rented
     end
-
-    def add_room(room_name)
-        @rooms << room_name.name
-    end
+    
+     def add_room(room_name)
+         @room_count += 1
+         if @room_count <= 4
+            @rooms << room_name.name        
+         else
+             p "I'm sorry, this apartment can't have more than 4 rooms."
+         end
+     end
 
     def list_rooms_by_name_alphabetically
         rooms.sort
