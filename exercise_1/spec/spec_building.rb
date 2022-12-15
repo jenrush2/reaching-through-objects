a3399363040b558b421d8e91e27b618a7a822563
Jennifer Rush <jennifer.rush2@gmail.com>
2022-12-14 18:57:55 -0700

    Test number and name

--- exercise_1/spec/spec_building.rb
+++ exercise_1/spec/spec_building.rb
@@ -3,3 +3,17 @@ require './lib/room'
 require './lib/apartment'
 require './lib/building'
 
+RSpec.describe Building do
+    it 'has a building number' do
+        building_1 = Building.new(623, "Savills Apartment Building")
+
+        expect(building_1.building_number).to eq(623)
+    end
+
+    it 'has a building name' do
+        building_1 = Building.new(623, "Savills Apartment Building")
+
+        expect(building_1.building_name).to eq("Savills Apartment Building")
+    end
+
+end
